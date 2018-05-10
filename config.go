902549101da66aa6e4b837710d63f24dc9d9d18a
Platonov Andrey <a.platonov@office.ngs.ru>
2018-05-10 14:27:18 +0700

    add coment to Config type

--- config.go
+++ config.go
@@ -1,5 +1,7 @@
 package powerdns
 
+// Config - represents config resource
+// https://doc.powerdns.com/md/httpapi/api_spec/#config95setting95resource
 type Config struct {
 	Name  string `json:"name"`
 	Type  string `json:"type"`
